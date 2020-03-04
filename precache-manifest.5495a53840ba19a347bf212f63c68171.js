10c773215ed12fcd1c3894f54f606519f758687e
rmodi234 <50226259+rmodi234@users.noreply.github.com>
2020-03-04 14:31:43 +0530

    Updates

---
+++ precache-manifest.5495a53840ba19a347bf212f63c68171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd874990897cca5591e0c9d6fc57b2a",
+    "url": "/apkmanagmentfront/index.html"
+  },
+  {
+    "revision": "41629de0a6cd67890b3f",
+    "url": "/apkmanagmentfront/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32ec5ce69ac0424eeb55",
+    "url": "/apkmanagmentfront/static/js/2.fb5f3459.chunk.js"
+  },
+  {
+    "revision": "8ce2385a9ea7439468621fb86ef6a20c",
+    "url": "/apkmanagmentfront/static/js/2.fb5f3459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41629de0a6cd67890b3f",
+    "url": "/apkmanagmentfront/static/js/main.3781390b.chunk.js"
+  },
+  {
+    "revision": "0c93cdf027aea2c9c66c",
+    "url": "/apkmanagmentfront/static/js/runtime-main.a7f5bcfe.js"
+  }
+]);
